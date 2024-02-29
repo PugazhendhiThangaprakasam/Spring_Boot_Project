3a270d4180ef135446ce1613290f04027409038a
Pugazhendhi T <124856051+PugazhendhiThangaprakasam@users.noreply.github.com>
2024-02-29 09:22:40 +0530

    Delete backend/realestate directory

--- backend/realestate/src/main/java/com/example/realestate/repository/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.realestate.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.realestate.model.Post;
-
-public interface PostRepository extends JpaRepository<Post,Integer>{
-    
-}
